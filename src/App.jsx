57d4b77f3891af08225758816880a0d9c3f6eee7
user <user@usernoMBP.lan>
2023-11-30 12:07:05 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { createBrowserRouter } from "react-router-dom";
+
+createBrowserRouter(); //array of objects. each object is one route.
+function App() {
+  return <>Hola, mi nombre es Eric Medrano</>;
+}
+export default App;
