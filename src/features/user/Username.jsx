23d5321d2c1ddde99816c36bd2dd932ddf29463d
user <user@usernoMBP.lan>
2023-12-02 21:30:51 +0900

    Implemented 2nd slice and other functionalities

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,9 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-  return <div className="hidden text-sm font-semibold md:block">Jonas</div>;
+  const user = useSelector((state) => state.user.username);
+  if (!user) return null;
+  return <div className="hidden text-sm font-semibold md:block">{user}</div>;
 }
 
 export default Username;
